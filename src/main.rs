7c416d0f608710d9c7db4d2fad7edd1fb10dede8
Ishara Abeykoon <isharaux@gmail.com>
2025-04-29 18:50:55 +1000

    Just the first few baby steps - Completing block

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use chrono::prelude::*;
+struct Transaction{}
+struct Block {
+    index: u64,
+    timestamp: String,
+    data: Vec<Transaction>,
+    previous_hash: String,
+    hash: String,
+    nonce: u64
+}
+
+
+
+impl Block {
+    fn new(index: u64,
+           previous_hash: String,
+           data: Vec<Transaction>) -> Self{
+        
+        let new_block = Block {
+            index: index,
+            timestamp: Utc::now().to_string(),
+            data,
+            previous_hash,
+            hash: String::new(),
+            nonce: 0
+        };
+        new_block
+        
+        
+    }
+}
 fn main() {
     println!("Hello, world!");
 }
