1e5ea9ca9cd8254529b71f1cceb40903fc05067d
Ishara Abeykoon <isharaux@gmail.com>
2025-05-28 19:03:37 +1000

    Adding CLI command execution

---
+++ src/cli.rs
@@ -0,0 +1,37 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(name = "CrabChain")]
+#[command(about = "A simple blockchain implementation in Rust", long_about = None)]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Commands,
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+    // New wallet generation
+    WalletNew,
+    WalletAddress{
+        #[arg(short, long)]
+        keyfile: String,
+    },
+    
+    Balances,
+    
+    Send{
+        #[arg(long)]
+        from: String,
+        #[arg(long)]
+        to: String,
+        #[arg(long)]
+        amount: u64
+    },
+    
+    
+    Mine{
+        #[arg(long)]
+        miner: String,
+
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,46 +3,79 @@ mod block;
 mod transaction;
 mod wallet;
 mod signed_transaction;
+mod cli;
 
+use clap::Parser;
 use ed25519_dalek::Verifier;
 use transaction::Transaction;
 use blockchain::Blockchain;
 use wallet::Wallet;
+use crate::cli::{Cli, Commands};
 use crate::signed_transaction::SignedTransaction;
 
+// fn main() {
+//     let mut crabchain = Blockchain::new();
+//     
+//     // Transaction will be in two steps now
+//     let sender_wallet = Wallet::new();
+//     
+//     let reward_tx = Transaction::new("SYSTEM".into(), "Alice".into(), 1000);
+//     let reward_tx_clone = reward_tx.clone();
+//     let reward_signed = SignedTransaction{
+//         transaction: reward_tx,
+//         public_key: sender_wallet.get_verifying_key(),
+//         signature: sender_wallet.sign_message(reward_tx_clone.to_string().as_bytes()),
+//     };
+//     crabchain.add_block(vec![reward_signed], 4, "Miner1".into());
+//     let tx1 = Transaction::new("Alice".into(), "Bob".into(), 100);
+//     let message = tx1.to_string();
+//     println!("{}", message);
+//     let sig = sender_wallet.sign_message(message.as_bytes());
+//     let signed_tx = SignedTransaction{
+//         transaction: tx1,
+//         public_key: sender_wallet.get_verifying_key(),
+//         signature: sig,
+//     };
+//     crabchain.add_block(vec![signed_tx], 4, "Miner1".into());
+// 
+//     println!("{:#?}", crabchain);
+// 
+//     let balances = crabchain.get_balances();
+//     println!("\nFinal balances:");
+//     for (user, balance) in balances {
+//         println!("{}: {}", user, balance);
+//     }
+// 
+// 
+// }
+
+// Moving into CLI control
 fn main() {
-    let mut crabchain = Blockchain::new();
+    let cli = Cli::parse();
     
-    // Transaction will be in two steps now
-    let sender_wallet = Wallet::new();
-    
-    let reward_tx = Transaction::new("SYSTEM".into(), "Alice".into(), 1000);
-    let reward_tx_clone = reward_tx.clone();
-    let reward_signed = SignedTransaction{
-        transaction: reward_tx,
-        public_key: sender_wallet.get_verifying_key(),
-        signature: sender_wallet.sign_message(reward_tx_clone.to_string().as_bytes()),
-    };
-    crabchain.add_block(vec![reward_signed], 4, "Miner1".into());
-    let tx1 = Transaction::new("Alice".into(), "Bob".into(), 100);
-    let message = tx1.to_string();
-    println!("{}", message);
-    let sig = sender_wallet.sign_message(message.as_bytes());
-    let signed_tx = SignedTransaction{
-        transaction: tx1,
-        public_key: sender_wallet.get_verifying_key(),
-        signature: sig,
-    };
-    crabchain.add_block(vec![signed_tx], 4, "Miner1".into());
-
-    println!("{:#?}", crabchain);
-
-    let balances = crabchain.get_balances();
-    println!("\nFinal balances:");
-    for (user, balance) in balances {
-        println!("{}: {}", user, balance);
+    match cli.command{
+        Commands::WalletNew => {
+            println!("Generating a new Wallet");
+        }
+        
+        Commands::WalletAddress { keyfile } => {
+            println!("Getting the address of the wallet {}", keyfile);
+        }
+        
+        Commands::Send { from, to, amount } => {
+            println!("Sending {} from {} to {}", amount, from, to);
+        }
+        
+        
+        Commands::Mine {miner} => {
+            println!("Mining block for {}", miner);
+        }
+        
+        Commands::Balances =>{
+            println!("Getting balances");
+        }
+        
     }
-
-
+    
+    
 }
-
